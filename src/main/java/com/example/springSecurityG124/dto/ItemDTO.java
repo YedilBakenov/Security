6a591ec70f5c5329d75ed47259efdd1c444f4049
Yedil Bakenov <yedil.bakenov.gmail.com>
2024-07-26 19:32:04 +0500

    changed ItemDTO file

--- src/main/java/com/example/springSecurityG124/dto/ItemDTO.java
+++ src/main/java/com/example/springSecurityG124/dto/ItemDTO.java
@@ -2,11 +2,13 @@ package com.example.springSecurityG124.dto;
 
 import lombok.Data;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Data
 @Getter
+@Setter
 public class ItemDTO {
 
     private int id;
